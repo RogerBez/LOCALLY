13d585c35a01e9c0ada4f69715b9795e6cb70137
RogerBez <rogerbez@gmail.com>
2025-03-19 10:05:34 +0200

    Fix build issues: remove unused import and disable CI mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import locallyBanner from './Assets/LOCALLY BANNER.jpg';
 import './App.css';
